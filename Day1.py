dbd98e2b81225ba9e58e92bcfa96670ab558220d
ClimateTech360 <obondopatrickotieno@gmail.com>
2025-03-11 13:01:05 +0300

    I have done my own practice

--- Day1.py
+++ Day1.py
@@ -1,3 +1,12 @@
 print("Hello blue")
 number=10
 print(number)
+temperature = 30  # Temperature in degrees Celsius
+
+if temperature > 25:
+    print("It's a hot day!")
+elif temperature > 15:
+    print("It's a warm day.")
+else:
+    print("It's a cold day.")
+    
